5a71ad9881b394a0db78f8c38d69825f49e7f9bc
Christopher Oakes <49435988+ChrisOakes@users.noreply.github.com>
2021-09-01 16:21:59 +0100

    test

--- NovaFori_Test/obj/Debug/netcoreapp3.1/NovaFori_Test.AssemblyInfo.cs
+++ NovaFori_Test/obj/Debug/netcoreapp3.1/NovaFori_Test.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("NovaFori, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\Chris Oakes\\source\\repos\\NovaFori\\NovaFori", "NovaFori.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("NovaFori, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\coake\\source\\repos\\Unwrapped\\ToDoList\\NovaFori", "NovaFori.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovaFori_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
